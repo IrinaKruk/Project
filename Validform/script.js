2c52d558b614a4f022d704195b7bf71096dc40ca
IrinaKruk <fatum2807@gmail.com>
2022-04-06 01:07:01 +0300

    add function of validation form

--- Validform/script.js
+++ Validform/script.js
@@ -1,104 +1,197 @@
 "use strict"
 
-function dynForm(form) {
+let elementForm = document.getElementById('form');
+elementForm.addEventListener('submit', formSubmit);
 
-   let newForm = document.createElement('form');
+let elementMan = document.getElementById('man');
+elementMan.addEventListener('blur', () => manValid(false));
 
-   newForm.method = "post";
-   newForm.action = "https://fe.it-academy.by/TestForm.php";
-   document.body.append(newForm);
-
-   for (let i = 0; i < form.length; i++) {
-      let f = form[i];
-      if ('caption' in f) {
-         let newCaption = document.createElement('input');
-         newCaption.type = f.kind;
-         newCaption.value = f.caption;
-         newForm.append(newCaption);
-      } else
-         if ('variants' in f && f.kind == 'radio') {
-            let newDiv = document.createElement('div');
-            newForm.append(newDiv);
-            let newLabel = document.createElement('label');
-            newLabel.innerHTML = f.label;
-            newDiv.append(newLabel);
-            for (let j = 0; j < f.variants.length; j++) {
-               //console.log(f.variants[j]['text']);
-               let newRadio = document.createElement('input');
-               newRadio.type = f.kind;
-               newRadio.name = f.name;
-               //console.log(newRadio);
-               newRadio.value = f.variants[j]['value'];
-               newDiv.append(newRadio);
-               //alert(f.variants[j]['text']);
-               let lab = document.createElement('label');
-               lab.innerHTML = f.label;
-               newDiv.appendChild(lab);
-               let newText = f.variants[j]['text'];
-               lab.innerHTML = newText;
-               //newForm.appendChild(lab);
-               //newRadio.insertAdjacentText('afterend', newText);
-            }
-         } else
-            if ('variants' in f && f.kind == 'combo') {
-               let newLabel = document.createElement('label');
-               newLabel.innerHTML = f.label;
-               newForm.append(newLabel);
-               let newSelect = document.createElement('select');
-               newSelect.name = f.name;
-               for (let j = 0; j < f.variants.length; j++) {
-                  //console.log(f.variants[j]['text']);
-                  //console.log(newSelect);
-                  let newOption = document.createElement('option');
-                  newOption.value = f.value;
-                  newOption.innerHTML = f.variants[j]['text'];
-                  newSelect.appendChild(newOption);
-               }
-               newForm.appendChild(newSelect);
-               newSelect.insertAdjacentHTML('afterend', '<br/>');
-            } else {
-               let newLabel = document.createElement('label');
-               newLabel.innerHTML = f.label;
-               newForm.append(newLabel);
-               newLabel.insertAdjacentHTML('afterend', '<br/>');
-               let newInput = document.createElement('input');
-               newInput.name = f.name;
-               newInput.type = f.kind;
-               if (f.kind == 'check') {
-                  newInput.type = 'checkbox';
-                  newInput.setAttribute('checked', 'true');
-               }
-               newLabel.append(newInput);
-            }
+function manValid(focusOnErr) {
+   let errCount = 0;
+   let manValue = document.getElementById('man').value;
+   let manElem = document.getElementById('man');
+   let manElemErr = document.getElementById('manErr');
+   if (manValue === '') {
+      manElemErr.innerHTML = 'Заполните поле';
+      errCount++;
+      console.log(errCount);
+      if (focusOnErr) {
+         manElem.focus();
+      }
+   } else {
+      manElemErr.innerHTML = '';
    }
+   return errCount;
 }
 
-let form1 = [
-   { label: 'Название сайта:', kind: 'longtext', name: 'sitename' },
-   { label: 'URL сайта:', kind: 'longtext', name: 'siteurl' },
-   { label: 'Посетителей в сутки:', kind: 'number', name: 'visitors' },
-   { label: 'E-mail для связи:', kind: 'shorttext', name: 'email' },
-   {
-      label: 'Рубрика каталога:', kind: 'combo', name: 'division',
-      variants: [{ text: 'здоровье', value: 1 }, { text: 'домашний уют', value: 2 }, { text: 'бытовая техника', value: 3 }]
-   },
-   {
-      label: 'Размещение:', kind: 'radio', name: 'payment',
-      variants: [{ text: 'бесплатное', value: 1 }, { text: 'платное', value: 2 }, { text: 'VIP', value: 3 }]
-   },
-   { label: 'Разрешить отзывы:', kind: 'check', name: 'votes' },
-   { label: 'Описание сайта:', kind: 'memo', name: 'description' },
-   { caption: 'Опубликовать', kind: 'submit' },
-];
+let elementName = document.getElementById('name');
+elementName.addEventListener('blur', () => nameValid(false));
 
-let form2 = [
-   { label: 'Фамилия:', kind: 'longtext', name: 'lastname' },
-   { label: 'Имя:', kind: 'longtext', name: 'firstname' },
-   { label: 'Отчество:', kind: 'longtext', name: 'secondname' },
-   { label: 'Возраст:', kind: 'number', name: 'age' },
-   { caption: 'Зарегистрироваться', kind: 'submit' },
-];
+function nameValid(focusOnErr) {
+   let errCount = 0;
+   let nameValue = document.getElementById('name').value;
+   let nameElem = document.getElementById('name');
+   if (nameValue === '') {
+      document.getElementById('nameErr').innerHTML = 'Заполните поле';
+      errCount++;
+      if (focusOnErr) {
+         nameElem.focus();
+      }
+   } else {
+      document.getElementById('nameErr').innerHTML = '';
+   }
+   return errCount;
+}
 
-dynForm(form1);
+let elementUrl = document.getElementById('url');
+elementUrl.addEventListener('blur', () => urlValid(false));
 
-dynForm(form2);
+function urlValid(focusOnErr) {
+   let errCount = 0;
+   let urlValue = document.getElementById('url').value;
+   let urlElem = document.getElementById('url');
+   if ((urlValue === '') || !(urlValue.includes('www'))) { /*!(urlValue.includes('.')) - не могу понять как добавить и это условие, и можно ли вообще...*/
+      document.getElementById('urlErr').innerHTML = 'Заполните поле';
+      errCount++;
+      if (focusOnErr) {
+         urlElem.focus();
+      }
+   } else {
+      document.getElementById('urlErr').innerHTML = '';
+   }
+   return errCount;
+}
+
+let elementDate = document.getElementById('date');
+elementDate.addEventListener('blur', () => dateValid(false));
+
+function dateValid(focusOnErr) {
+   let errCount = 0;
+   let dateValue = document.getElementById('date').value;
+   let dateElem = document.getElementById('date');
+   if (dateValue === '') {
+      document.getElementById('dateErr').innerHTML = 'Заполните поле';
+      errCount++;
+      if (focusOnErr) {
+         dateElem.focus();
+      }
+   } else {
+      document.getElementById('dateErr').innerHTML = '';
+   }
+   return errCount;
+}
+
+let elementVisitor = document.getElementById('visitor');
+elementVisitor.addEventListener('blur', () => visitorValid(false));
+
+function visitorValid(focusOnErr) {
+   let errCount = 0;
+   let visitorValue = document.getElementById('visitor').value;
+   console.log(typeof visitorValue);
+   let visitorElem = document.getElementById('visitor');
+   if (visitorValue === '' || isNaN(visitorValue)) {
+      document.getElementById('visitorErr').innerHTML = 'Заполните поле';
+      errCount++;
+      if (focusOnErr) {
+         visitorElem.focus();
+      }
+   } else {
+      document.getElementById('visitorErr').innerHTML = '';
+   }
+   return errCount;
+}
+
+let elementMail = document.getElementById('mail');
+elementMail.addEventListener('blur', () => mailValid(false));
+
+function mailValid(focusOnErr) {
+   let errCount = 0;
+   let mailValue = document.getElementById('mail').value;
+   let mailElem = document.getElementById('mail');
+   if ((mailValue === '') || !(mailValue.includes('@'))) { /*!(mailValue.includes('.')) - не могу понять как добавить и это условие, и можно ли вообще...*/
+      document.getElementById('mailErr').innerHTML = 'Заполните поле';
+      errCount++;
+      if (focusOnErr) {
+         mailElem.focus();
+      }
+   } else {
+      document.getElementById('mailErr').innerHTML = '';
+   }
+   return errCount;
+}
+
+/*
+let elementRadio = document.getElementById('radio');
+elementRadio.addEventListener('blur', () => radioValid(false));
+
+function radioValid(focusOnErr) {
+   let errCount = 0;
+   let radioElem = document.getElementById('radio');
+
+   console.log(radioElem);
+   if () {
+      document.getElementById('radioErr').innerHTML = 'Заполните поле';
+      errCount++;
+      if (focusOnErr) {
+         radioElem.focus();
+      }
+   } else {
+      document.getElementById('radioErr').innerHTML = '';
+   }
+   return errCount;
+}
+*/
+
+let elementCheck = document.getElementById('check');
+elementCheck.addEventListener('blur', () => checkValid(false));
+
+function checkValid(focusOnErr) {
+   let errCount = 0;
+   let checkElem = document.getElementById('check');
+   if (checkElem.checked === false) {
+      document.getElementById('checkErr').innerHTML = 'Заполните поле';
+      errCount++;
+      if (focusOnErr) {
+         checkElem.focus();
+      }
+   } else {
+      document.getElementById('checkErr').innerHTML = '';
+   }
+   return errCount;
+}
+
+let elementText = document.getElementById('text');
+elementText.addEventListener('blur', () => textValid(false));
+
+function textValid(focusOnErr) {
+   let errCount = 0;
+   let textValue = document.getElementById('text').value;
+   let textElem = document.getElementById('text');
+   if (textValue === '') {
+      document.getElementById('textErr').innerHTML = 'Заполните поле';
+      errCount++;
+      if (focusOnErr) {
+         textElem.focus();
+      }
+   } else {
+      document.getElementById('textErr').innerHTML = '';
+   }
+   return errCount;
+}
+
+
+function formSubmit(eo) {
+   let errCount = 0;
+   errCount += manValid(!errCount);
+   errCount += nameValid(!errCount);
+   errCount += urlValid(!errCount);
+   errCount += dateValid(!errCount);
+   errCount += visitorValid(!errCount);
+   errCount += mailValid(!errCount);
+   errCount += checkValid(!errCount);
+   //errCount += radioValid(!errCount);
+   errCount += textValid(!errCount);
+   if (errCount) {
+      eo.preventDefault();
+   }
+}
